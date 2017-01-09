546d48d9d852ee65b213b163513127ca9f7febca
KamilSiwecki <siweckikamil@gmail.com>
2017-01-09 01:29:20 +0100

    zmiany value added przy podkrecaniu

--- wiot.R
+++ wiot.R
@@ -40,6 +40,89 @@ Z<-A
 print(excel[5,1855])
 print(K[1849,1849])
 
+#### lista krajow####
+
+LKw<-excel[7:2470,3]
+print(LKw[100])
+LKk<-excel[5,5:2468]
+print(LKk[1900])
+
+
+#### przepływy wybrany kraj -> wybrany kraj ##########
+
+print(nk<-K[LKw=="POL",LKk=="POL"])
+
+print(startW<-row.names(nk)[1])
+stw<-as.double(startW)-6
+mode(st) # tutaj znaleźliśmy, dla jakiego indeksu zaczynamy w kraju z ktorego idzie przepływ
+
+print(startK<-row.names(nk)[1])
+stk<-as.double(startK)-6
+print(stk) # tutaj znalezlismy pierwszy indeks kraju, do ktorego idzie przepływ
+
+###test wykresu
+
+A[stw-1,stw+3]=(A[stw-1,stk+3]+0.01)
+W<-solve(I-A)%*%Y
+W<-as.vector(W)
+E<-t(t(A)*W)
+s<-colSums(E)
+v<-W-s
+roz<-(vaa-v)
+print(paste(excel[stw+5,3],  excel[stw+5,2], "to sector",excel[5,stk+3], excel[4,stk+3]))
+print(wykr<-roz[stk:(stk+55)])
+plot(wykr)
+barplot(wykr)
+
+
+n=1
+przeplyw<-vector()
+procVA<-vector()
+procVAa<-vector()
+dVA<-vector() ### wektor zmiany procentowego udzialu warotsci dodanej PL w stos do VA calego swiata
+
+
+for (i in 1:5){
+  for(j in 1:5){
+    A[stw-1+i,stw-1+j]=(A[stw-1+i,stk-1+j]-0.01)
+    W<-solve(I-A)%*%Y
+    W<-as.vector(W)
+    E<-t(t(A)*W)
+    s<-colSums(E)
+    v<-W-s
+    roz<-(v-vaa)
+    print(przeplyw[n]<-(paste(excel[stw+5+i,3],  excel[stw+5+i,2], "to sector",excel[5,stk+3+j], excel[4,stk+3+j])))
+    procVA[n]<-(sum(v[stk:(stk+55)]))
+    all<-sum(v)
+    print(a<-(procVA[n]/all))
+    
+    procVAa[n]<-(sum(vaa[stk:(stk+55)]))
+    ALL<-sum(vaa)
+    print(b<-(procVAa[n]/ALL))
+    
+    print(dVA[n]<-100*(a-b)/b) ## *100 zeby bylo w procentach
+    A<-Z #to dlatego, żeby po każdej zmianie macierzy A wracał do jej początkowej wartości
+    # przed kolejnym obiegiem petli
+    n=n+1
+    
+  }
+  
+}
+
+dVA=dVA*100
+
+namArg=vector()
+
+namArg=paste(rep((1:5), times=c(5,5,5,5,5)),rep(c(1:5),5))
+print(namArg)
+
+barplot(dVA[1:25],col=rainbow(5),xlab="przepływy" ,names.arg = namArg, main="Wykres zmian wartosci dodanej w efekcie zmiany przeplywu")
+
+
+
+
+
+#### JUŻ WYBRANE PRZEPŁYWY -  DLA POLSKI#####
 
 #### PRZEPLYWY DO SEKTORÓW POLSKICH ####
 for (i in 1:2464){
